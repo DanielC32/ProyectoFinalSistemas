662d34332dbf7452a16265cde71d1b9a0b476c85
unknown <samuelitomr@yahoo.com>
2024-09-30 21:25:49 -0500

    hice cambios rey

--- constants/images.js
+++ constants/images.js
@@ -5,5 +5,14 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import ej1 from "../assets/images/ej1.png";
+import ej2 from "../assets/images/ej2.png";
+import ej3 from "../assets/images/ej3.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export const images = {
+    ej1: require('../assets/images/ej1.png'),
+    ej2: require('../assets/images/ej2.png'),
+    ej3: require('../assets/images/ej3.png'),
+    // Añade todas las imágenes que necesites
+  };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, ej1, ej2, ej3 };
